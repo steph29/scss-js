ac874b49fbe70d2e0d20f6264548ea552d1da162
steph29 <s.verardo29@gmail.com>
2020-11-21 16:04:06 +0100

    done js animation

--- script/script.js
+++ script/script.js
@@ -1,5 +1,25 @@
-const question = document.querySelector("question");
+disappearOffScreen(1);
+disappearOffScreen(2);
+disappearOffScreen(3);
+disappearOffScreen(4);
+disappearOffScreen(5);
 
-question.addEventListener("click", function () {
-  question.childNodes("img") = trans;
-});
+function disappearOffScreen(i) {
+  let toogle = document.getElementById(`tog${i}`);
+  let res = document.getElementById(`res${i}`);
+  let title = document.getElementById(`title${i}`);
+  let icon = document.getElementById(`icon${i}`);
+  toogle.addEventListener("click", () => {
+    if (getComputedStyle(res).display != "none") {
+      res.style.display = "none";
+      title.style.color = "#787887";
+      title.style.fontWeight = "400";
+      icon.style.rotate = "360deg";
+    } else {
+      res.style.display = "block";
+      icon.style.rotate = "180deg";
+      title.style.color = "#1d1e35";
+      title.style.fontWeight = "700";
+    }
+  });
+}
